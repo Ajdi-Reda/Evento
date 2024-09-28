7f7e198ca2c4a8e94bf0b6562613e1622bbfd7e1
Reda Ajdi <redaajdi6@gmail.com>
2024-09-28 12:01:19 +0200

    page params for dynamic H1

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,10 +1,21 @@
 import H1 from "@/components/h1";
 import React from "react";
 
-const CityEvents = () => {
+type cityEventsProps = {
+  params: {
+    city: string;
+  };
+};
+
+const CityEvents = ({ params }: cityEventsProps) => {
+  const { city } = params;
   return (
-    <main>
-      <H1>Events in Austin</H1>
+    <main className="flex min-h-[110vh] flex-col items-center px-[20px] py-24">
+      <H1>
+        {city === "all"
+          ? "All events"
+          : `Event in ${city.charAt(0).toUpperCase() + city.slice(1)}`}
+      </H1>
     </main>
   );
 };
