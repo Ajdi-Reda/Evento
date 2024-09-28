2101c6e6b0d7c3ea3937c1dbc805004cf059f007
Reda Ajdi <redaajdi6@gmail.com>
2024-09-28 10:16:11 +0200

    creating a reusable h1 component

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,7 +1,12 @@
+import H1 from "@/components/h1";
 import React from "react";
 
 const CityEvents = () => {
-  return <main>All city events</main>;
+  return (
+    <main>
+      <H1>Events in Austin</H1>
+    </main>
+  );
 };
 
 export default CityEvents;
